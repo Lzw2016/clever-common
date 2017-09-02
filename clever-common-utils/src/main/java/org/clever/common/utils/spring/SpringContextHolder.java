2ec755549919ac8c250735dd2e891f39c49203a6
Lzw2016 <1183409807@qq.com>
2017-09-02 23:39:02 +0800

    重构

--- clever-common-utils/src/main/java/org/clever/common/utils/spring/SpringContextHolder.java
+++ clever-common-utils/src/main/java/org/clever/common/utils/spring/SpringContextHolder.java
@@ -1,15 +1,10 @@
 package org.clever.common.utils.spring;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.DisposableBean;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
 import org.springframework.core.io.DefaultResourceLoader;
-import org.springframework.stereotype.Component;
 import org.springframework.web.context.ContextLoader;
 import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.context.support.WebApplicationContextUtils;
-import org.springframework.web.servlet.FrameworkServlet;
 
 import javax.servlet.ServletContext;
 
@@ -23,18 +18,12 @@ import javax.servlet.ServletContext;
  * 创建时间：2016-5-9 14:25 <br/>
  */
 @Slf4j
-@Component
-public class SpringContextHolder implements ApplicationContextAware, DisposableBean {
+public class SpringContextHolder {
 
     /**
      * Spring ApplicationContext容器
      */
     private static ApplicationContext applicationContext = null;
-
-    /**
-     * Spring ApplicationContext容器的子容器
-     */
-    private static WebApplicationContext webApplicationContext = null;
 
     /**
      * Servlet容器的上下文
@@ -44,20 +33,11 @@ public class SpringContextHolder implements ApplicationContextAware, DisposableB
     /**
      * 实现ApplicationContextAware接口, 注入Context到静态变量中.
      */
-    @Override
-    public void setApplicationContext(ApplicationContext applicationContext) {
+    public static void setApplicationContext(ApplicationContext applicationContext) {
         if (SpringContextHolder.applicationContext != null) {
             log.info("SpringContextHolder中的ApplicationContext被覆盖, 原有ApplicationContext为:" + SpringContextHolder.applicationContext);
         }
         SpringContextHolder.applicationContext = applicationContext;
-    }
-
-    /**
-     * 实现DisposableBean接口, 在Context关闭时清理静态变量.
-     */
-    @Override
-    public void destroy() throws Exception {
-        applicationContext = null;
     }
 
     /**
@@ -131,45 +111,5 @@ public class SpringContextHolder implements ApplicationContextAware, DisposableB
             servletContext = webApplicationContext.getServletContext();
         }
         return servletContext;
-    }
-
-    /**
-     * @return Spring ApplicationContext容器的子容器
-     */
-    public static WebApplicationContext getWebApplicationContext() {
-        if (webApplicationContext == null) {
-            String attrName = FrameworkServlet.SERVLET_CONTEXT_PREFIX + "springServlet";
-            webApplicationContext = WebApplicationContextUtils.getWebApplicationContext(getServletContext(), attrName);
-        }
-        return webApplicationContext;
-    }
-
-    /**
-     * 从静态变量applicationContext中取得Bean, 自动转型为所赋值对象的类型.
-     *
-     * @return 返回Bean对象，失败返回null
-     */
-    @SuppressWarnings("unchecked")
-    public static <T> T getWebBean(String name) {
-        try {
-            return (T) getWebApplicationContext().getBean(name);
-        } catch (Throwable e) {
-            log.error("获取Bean失败 name=" + name, e);
-            return null;
-        }
-    }
-
-    /**
-     * 从静态变量applicationContext中取得Bean, 自动转型为所赋值对象的类型.
-     *
-     * @return 返回Bean对象，失败返回null
-     */
-    public static <T> T getWebBean(Class<T> requiredType) {
-        try {
-            return getWebApplicationContext().getBean(requiredType);
-        } catch (Throwable e) {
-            log.error("获取Bean失败 class=" + requiredType, e);
-            return null;
-        }
     }
 }
