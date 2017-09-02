35ec69762cb0a7468cf21f059353618fecd144dd
Lzw2016 <1183409807@qq.com>
2017-09-02 21:33:32 +0800

    重构

---
+++ clever-common-utils/src/main/java/org/clever/common/utils/ConversionUtils.java
@@ -0,0 +1,44 @@
+package org.clever.common.utils;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.Conversion;
+
+/**
+ * 对象类型转换工具类，使用类型强制转换实现<br/>
+ * 1.把一种类型转换成另一种类型<br/>
+ *
+ * @author LiZhiWei
+ * @version 2015年6月21日 上午11:32:12
+ */
+@Slf4j
+public class ConversionUtils extends Conversion {
+    /**
+     * 把一种类型，通过类型强制转换成另一种类型，转换失败抛出异常<br/>
+     *
+     * @param object 待转换的数据
+     */
+    @SuppressWarnings("unchecked")
+    public static <E> E converter(Object object) {
+        return (E) object;
+    }
+
+    /**
+     * 把一种类型，通过类型强制转换成另一种类型，转换失败不会抛出异常<br/>
+     * 1.注意：defaultValue与返回值类型一定要一致<br/>
+     *
+     * @param object       待转换的数据
+     * @param defaultValue 转换失败返回的默认值
+     */
+    @SuppressWarnings("unchecked")
+    public static <E> E converter(Object object, E defaultValue) {
+        if (object == null) {
+            return defaultValue;
+        }
+        try {
+            return (E) object;
+        } catch (Throwable e) {
+            log.error(e.getMessage(), e);
+            return defaultValue;
+        }
+    }
+}
---
+++ clever-common-utils/src/main/java/org/clever/common/utils/IPAddressUtils.java
@@ -0,0 +1,94 @@
+package org.clever.common.utils;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.util.Enumeration;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 获取本机IP地址
+ * <p/>
+ * 作者：LiZW <br/>
+ * 创建时间：2016-8-4 16:22 <br/>
+ */
+@Slf4j
+public class IPAddressUtils {
+    /**
+     * 本机所有的IPv4地址
+     */
+    private static Set<String> Inet4AddressSet = null;
+
+    /**
+     * 获取本机所有的IPv4地址
+     *
+     * @return 失败返回null
+     */
+    private static Set<String> getAllIPv4() {
+        Set<String> inet4Address = new HashSet<>();
+        try {
+            Enumeration<NetworkInterface> allNetInterfaces = NetworkInterface.getNetworkInterfaces();
+            InetAddress ip;
+            while (allNetInterfaces.hasMoreElements()) {
+                NetworkInterface netInterface = allNetInterfaces.nextElement();
+                Enumeration<InetAddress> addresses = netInterface.getInetAddresses();
+                while (addresses.hasMoreElements()) {
+                    ip = addresses.nextElement();
+                    if (ip != null && ip instanceof Inet4Address) {
+                        inet4Address.add(ip.getHostAddress());
+                    }
+                }
+            }
+        } catch (Throwable e) {
+            log.error("### 获取Inet4Address失败", e);
+            return null;
+        }
+        return inet4Address;
+    }
+
+    /**
+     * 获取本机所有的IPv4地址,已经获取过就直接返回
+     *
+     * @return 失败返回null
+     */
+    public static Set<String> getInet4Address() {
+        if (Inet4AddressSet == null) {
+            Inet4AddressSet = getAllIPv4();
+        }
+        return Inet4AddressSet;
+    }
+
+    /**
+     * 重新获取本机所有的IPv4地址
+     *
+     * @return 失败返回null
+     */
+    public static Set<String> refreshInet4Address() {
+        Inet4AddressSet = getAllIPv4();
+        return Inet4AddressSet;
+    }
+
+    /**
+     * 获取本机所有的IPv4地址,有多个用“;”分隔
+     *
+     * @return 失败返回"未知"
+     */
+    public static String getInet4AddressStr() {
+        Set<String> inet4Address = getInet4Address();
+        if (inet4Address != null) {
+            StringBuilder stringBuilder = new StringBuilder();
+            for (String str : inet4Address) {
+                if (org.apache.commons.lang3.StringUtils.isNotBlank(stringBuilder.toString())) {
+                    stringBuilder.append(';');
+                }
+                stringBuilder.append(str);
+            }
+            return stringBuilder.toString();
+        } else {
+            return "未知";
+        }
+    }
+}
