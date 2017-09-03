5a0dac717c890968dd5b41810ea14dfeeb6fceb1
Lzw2016 <1183409807@qq.com>
2017-09-03 18:45:40 +0800

    重构

---
+++ clever-common-server/src/main/java/org/clever/common/server/mapper/CustomMapper.java
@@ -0,0 +1,12 @@
+package org.clever.common.server.mapper;
+
+import tk.mybatis.mapper.common.Mapper;
+
+/**
+ * 自定义的Mapper扩展通用Mapper
+ * <p>
+ * 作者：lzw <br/> Custom
+ * 创建时间：2017-09-03 14:18 <br/>
+ */
+public interface CustomMapper<T> extends Mapper<T> {
+}
