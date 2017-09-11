c7913a22f344afde2d54831e597f493593000e3d
Lzw2016 <lizhiwei1993>
2017-09-11 10:42:17 +0800

    重构

--- clever-common-model/src/main/java/org/clever/common/model/exception/ErrorCode.java
+++ clever-common-model/src/main/java/org/clever/common/model/exception/ErrorCode.java
@@ -1,10 +1,38 @@
 package org.clever.common.model.exception;
 
+import lombok.Getter;
+
 /**
  * 系统错误码
  * <p>
  * 作者：lzw <br/>
  * 创建时间：2017-09-03 11:43 <br/>
  */
+@Getter
 public enum ErrorCode {
+
+    ;
+    /**
+     * 错误码
+     */
+    private int code;
+
+    /**
+     * 错误消息
+     */
+    private String message;
+
+    ErrorCode(int code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public static String getName(int code) {
+        for (ErrorCode errorCode : ErrorCode.values()) {
+            if (errorCode.code == code) {
+                return errorCode.message;
+            }
+        }
+        return null;
+    }
 }
