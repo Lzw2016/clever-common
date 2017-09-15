301856802b5286e8cc94106006d6fed541efd744
Lzw2016 <lizhiwei1993>
2017-09-15 21:25:51 +0800

    重构

---
+++ clever-common-client/src/main/java/org/clever/common/client/BaseClient.java
@@ -0,0 +1,13 @@
+package org.clever.common.client;
+
+/**
+ * 作者：lizw <br/>
+ * 创建时间：2017/9/15 21:06 <br/>
+ */
+public interface BaseClient {
+
+    /**
+     * 视图页面(JSP)的后缀
+     */
+    String JSON_SUFFIX = ".json";
+}
---
+++ clever-common-client/src/main/java/org/clever/common/fallback/FallbackClient.java
@@ -0,0 +1,10 @@
+package org.clever.common.fallback;
+
+import org.clever.common.client.BaseClient;
+
+/**
+ * 作者：lizw <br/>
+ * 创建时间：2017/9/15 21:07 <br/>
+ */
+public class FallbackClient implements BaseClient {
+}
