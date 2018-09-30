c975a138390368fcd9bc6c5b96df92cac302e141
Lzw2016 <lizhiwei1993>
2018-09-30 21:54:55 +0800

    优化

--- clever-common-server/src/main/java/org/clever/common/server/swagger/DefaultSwagger2Config.java
+++ clever-common-server/src/main/java/org/clever/common/server/swagger/DefaultSwagger2Config.java
@@ -16,7 +16,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  */
 @Configuration
 @EnableSwagger2
-public class Swagger2Config {
+public class DefaultSwagger2Config {
 
     @Bean
     public Docket createBootApi() {
