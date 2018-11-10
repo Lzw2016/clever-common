6bd53cf0f0939c8cae3bdcaad8f4e2b22d68f697
Lzw2016 <lizhiwei1993>
2018-11-10 19:16:06 +0800

    新增 全局任务处理

---
+++ clever-common-utils/src/main/java/org/clever/common/utils/GlobalJob.java
@@ -0,0 +1,43 @@
+package org.clever.common.utils;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 全局任务处理，JVM内同一时刻只有一个线程执行，直接跳过多余调用
+ * <p>
+ * 作者： lzw<br/>
+ * 创建时间：2018-11-10 19:03 <br/>
+ */
+
+@Slf4j
+public abstract class GlobalJob {
+
+    private static boolean LOCK = false;
+
+    /**
+     * 全局执行的任务(JVM内同一时刻只有一个线程执行)
+     */
+    protected abstract void internalExecute();
+
+    /**
+     * 任务执行的异常处理
+     */
+    protected abstract void exceptionHandle(Throwable e);
+
+    /**
+     * 任务处理内部逻辑
+     */
+    public void execute() {
+        if (LOCK) {
+            return;
+        }
+        try {
+            LOCK = true;
+            internalExecute();
+        } catch (Throwable e) {
+            exceptionHandle(e);
+        } finally {
+            LOCK = false;
+        }
+    }
+}
