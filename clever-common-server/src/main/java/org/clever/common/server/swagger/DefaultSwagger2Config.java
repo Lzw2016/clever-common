8835ccd965ffbc519b8fbb61fd2795d93fa6c06b
lizw <lzw1000000@163.com>
2019-04-25 17:26:58 +0800

    小优化

--- clever-common-server/src/main/java/org/clever/common/server/swagger/DefaultSwagger2Config.java
+++ clever-common-server/src/main/java/org/clever/common/server/swagger/DefaultSwagger2Config.java
@@ -2,6 +2,7 @@ package org.clever.common.server.swagger;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -14,6 +15,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  * 作者： lzw<br/>
  * 创建时间：2018-09-30 14:17 <br/>
  */
+@Profile({"dev", "test"})
 @Configuration
 @EnableSwagger2
 public class DefaultSwagger2Config {
