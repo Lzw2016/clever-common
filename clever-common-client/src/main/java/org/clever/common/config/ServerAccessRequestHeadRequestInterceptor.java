a1435ddb741be3dcc97ab33a9f9a8460e1d2fb72
lizw <lzw1000000@163.com>
2019-05-17 09:34:12 +0800

    ...

--- clever-common-client/src/main/java/org/clever/common/config/ServerAccessRequestHeadRequestInterceptor.java
+++ clever-common-client/src/main/java/org/clever/common/config/ServerAccessRequestHeadRequestInterceptor.java
@@ -26,10 +26,10 @@ public class ServerAccessRequestHeadRequestInterceptor implements RequestInterce
     public void apply(RequestTemplate template) {
         if (requestHeadConfig == null) {
             requestHeadConfig = SpringContextHolder.getBean(ServerAccessRequestHeadConfig.class);
-            log.info("读取服务之间访问的全局请求头设置: {}", requestHeadConfig.getHeads());
+            // log.debug("读取服务之间访问的全局请求头设置: {}", requestHeadConfig.getHeads());
         }
         if (requestHeadConfig.getHeads() != null && requestHeadConfig.getHeads().size() > 0) {
-            log.debug("[访问服务的全局请求头] --> {}", requestHeadConfig.getHeads());
+            // log.debug("[访问服务的全局请求头] --> {}", requestHeadConfig.getHeads());
             for (Map.Entry<String, String> entry : requestHeadConfig.getHeads().entrySet()) {
                 template.header(entry.getKey(), entry.getValue());
             }
