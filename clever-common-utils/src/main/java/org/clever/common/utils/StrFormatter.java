bdf2ffc4adbc98e0f4d6da217ea3747438a93ab9
lizw <lzw1000000@163.com>
2020-07-10 13:21:24 +0800

    ... 小优化

--- clever-common-utils/src/main/java/org/clever/common/utils/StrFormatter.java
+++ clever-common-utils/src/main/java/org/clever/common/utils/StrFormatter.java
@@ -316,7 +316,7 @@ public class StrFormatter {
             return StringUtils.EMPTY;
         }
         int len = param.length();
-        StringBuilder sb = new StringBuilder(len);
+        StringBuilder sb = new StringBuilder(len + 16);
         for (int i = 0; i < len; i++) {
             char c = param.charAt(i);
             if (Character.isUpperCase(c) && i > 0) {
