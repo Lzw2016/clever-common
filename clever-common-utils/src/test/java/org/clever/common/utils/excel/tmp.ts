a348480cd373713aa1230b3b1a2f0af13415d020
lizw <lzw1000000@163.com>
2020-08-14 10:03:15 +0800

    升级 easyexcel 28%

--- clever-common-utils/src/test/java/org/clever/common/utils/excel/tmp.ts
+++ clever-common-utils/src/test/java/org/clever/common/utils/excel/tmp.ts
@@ -14,16 +14,6 @@ interface GlobalConfiguration {
 
 }
 
-interface Converter<T> {
-    supportJavaTypeKey(): Class;
-
-    supportExcelTypeKey(): CellDataTypeEnum;
-
-    convertToJavaData(cellData: CellData, contentProperty: ExcelContentProperty, globalConfiguration: GlobalConfiguration): T;
-
-    convertToExcelData(value: T, contentProperty: ExcelContentProperty, globalConfiguration: GlobalConfiguration): CellData;
-}
-
 enum CellExtraTypeEnum {
     /** 批注信息 */
     COMMENT = "COMMENT",
@@ -62,29 +52,32 @@ enum Locale {
 }
 
 interface AbstractParameterBuilder<T extends AbstractParameterBuilder<T>> {
-
+    /** 如果日期使用1904窗口，则为True；如果使用1900日期窗口，则为false */
     use1904windowing(use1904windowing: boolean): T;
 
+    /** Locale对象表示特定的地理、政治或文化区域。设置日期和数字格式时使用此参数 */
     locale(locale: Locale);
 
+    /** 自动删除空格字符 */
     autoTrim(autoTrim: boolean);
-
-    // registerConverter<M>(converter: Converter<M>);
 }
 
 interface AbstractExcelReaderParameterBuilder<T extends AbstractExcelReaderParameterBuilder<T>> extends AbstractParameterBuilder<T> {
 
+    /** 表头行数 */
     headRowNumber(headRowNumber: number): T;
 
+    /** 使用科学格式 */
     useScientificFormat(useScientificFormat: boolean): T;
 
     // registerReadListener(readListener: ReadListener): T;
 }
 
 interface ExcelReaderSheetBuilder extends AbstractExcelReaderParameterBuilder<ExcelReaderSheetBuilder> {
-
+    /** 页签编号(从0开始) */
     sheetNo(sheetNo: number): ExcelReaderSheetBuilder;
 
+    /** 页签名称(xlsx格式才支持) */
     sheetName(sheetName: string): ExcelReaderSheetBuilder;
 
     /**
@@ -99,18 +92,26 @@ interface ExcelReaderSheetBuilder extends AbstractExcelReaderParameterBuilder<Ex
 }
 
 interface ExcelReaderBuilder extends AbstractExcelReaderParameterBuilder<ExcelReaderBuilder> {
+    /** 文件输入流 */
+
     // file(inputStream: InputStream): ExcelReaderBuilder;
 
+    /** 强制使用输入流，如果为false，则将“inputStream”传输到临时文件以提高效率 */
     mandatoryUseInputStream(mandatoryUseInputStream: boolean): ExcelReaderBuilder;
 
+    /** 是否自动关闭输入流 */
     autoCloseStream(autoCloseStream: boolean): ExcelReaderBuilder;
 
+    /** 是否忽略空行 */
     ignoreEmptyRow(ignoreEmptyRow: boolean): ExcelReaderBuilder;
 
+    /** 设置一个自定义对象，可以在侦听器中读取此对象(AnalysisContext.getCustom()) */
     customObject(customObject: any): ExcelReaderBuilder;
 
+    /** Excel文件密码 */
     password(password: string): ExcelReaderBuilder;
 
+    /** 读取扩展信息配置 */
     extraRead(extraRead: CellExtraTypeEnum): ExcelReaderBuilder;
 
     /**
@@ -125,32 +126,54 @@ interface ExcelReaderBuilder extends AbstractExcelReaderParameterBuilder<ExcelRe
      */
     sheet(sheetName: string): ExcelReaderSheetBuilder;
 
+    /** 开始读取所有的页签数据 */
     doReadAll(): void;
 
+    /** 开始读取所有的页签数据，并返回所有结果(数据量大时，会消耗大量内存) */
     // doReadAllSync(): List<T>;
 }
 
+enum DataType {
+    String = "String",
+    BigDecimal = "BigDecimal",
+    Boolean = "Boolean",
+    ByteArray = "Byte[]",
+    Byte = "Byte",
+    Date = "Date",
+    Double = "Double",
+    Float = "Float",
+    Integer = "Integer",
+    Long = "Long",
+    Short = "Short",
+}
+
 interface ExcelProperty {
+    /** 列名称 */
+    column: string | string[];
+
+    /** 列的数据类型 */
+    dataType: DataType;
+
+    /** 是否忽略当前列 */
     ignore?: boolean;
 
-    column?: string | string[];
-
-    index?: number;
-
+    /** 定义列的排序顺序 */
     order?: number;
-
-    // converter
 }
 
 interface DateTimeFormat {
-    value: string;
+    /** 时间格式化的格式定义 */
+    dateFormat: string;
 
+    /** 如果日期使用1904窗口，则为True；如果使用1900日期窗口，则为false */
     use1904windowing?: boolean;
 }
 
 interface NumberFormat {
-    value: string;
+    /** 数字格式化 */
+    numberFormat: string;
 
+    /** 四舍五入模式 */
     // roundingMode?: RoundingMode;
 }
 
@@ -164,43 +187,62 @@ interface NumberFormat {
 // HeadStyle
 // OnceAbsoluteMerge
 
-interface ExcelReaderHeadConfig extends ExcelProperty {
-    dateTimeFormat?: DateTimeFormat;
-
-    numberFormat?: NumberFormat;
+interface ExcelReaderHeadConfig extends ExcelProperty, Partial<DateTimeFormat>, Partial<NumberFormat> {
 }
 
 class ExcelReaderConfig<T extends object> {
-
+    /** 文件输入流 */
     // file?: InputStream;
 
-    autoCloseStream: boolean = false;
+    /** 是否自动关闭输入流 */
+    autoCloseStream?: boolean = false;
 
-    extraRead: CellExtraTypeEnum[] = [];
+    /** 读取扩展信息配置 */
+    extraRead?: CellExtraTypeEnum[] = [];
 
-    ignoreEmptyRow: boolean = false;
+    /** 是否忽略空行 */
+    ignoreEmptyRow?: boolean = false;
 
-    mandatoryUseInputStream: boolean = false;
+    /** 强制使用输入流，如果为false，则将“inputStream”传输到临时文件以提高效率 */
+    mandatoryUseInputStream?: boolean = false;
 
+    /** Excel文件密码 */
     password?: string;
 
+    /** Excel页签 */
     sheet: number | string = 0;
 
+    /** 表头行数 */
     headRowNumber: number = 1;
 
-    useScientificFormat: boolean = false;
+    /** 使用科学格式 */
+    useScientificFormat?: boolean = false;
 
-    registerConverter: any[] = [];
+    /** 如果日期使用1904窗口，则为True；如果使用1900日期窗口，则为false */
+    use1904windowing?: boolean = false;
 
-    use1904windowing: boolean = false;
+    /** Locale对象表示特定的地理、政治或文化区域。设置日期和数字格式时使用此参数 */
+    locale?: Locale = Locale.SIMPLIFIED_CHINESE;
 
-    locale: Locale = Locale.SIMPLIFIED_CHINESE;
+    /** 自动删除空格字符 */
+    autoTrim?: boolean = true;
 
-    autoTrim: boolean = true;
+    /** 设置一个自定义对象，可以在侦听器中读取此对象(AnalysisContext.getCustom()) */
+    customObject?: any;
 
-    heads?: {
-        [head in keyof T]: ExcelReaderHeadConfig;
+    /** Excel列配置(表头) */
+    columns?: {
+        [column in keyof T]: ExcelReaderHeadConfig;
     };
+
+    constructor(sheet?: number | string, headRowNumber?: number, autoTrim?: boolean, ignoreEmptyRow?: boolean, locale?: Locale, password?: string) {
+        this.sheet = sheet ?? 0;
+        this.headRowNumber = headRowNumber ?? 1;
+        this.autoTrim = autoTrim ?? true;
+        this.ignoreEmptyRow = ignoreEmptyRow ?? false;
+        this.locale = locale ?? Locale.SIMPLIFIED_CHINESE;
+        this.password = password ?? null;
+    }
 }
 
 interface ExcelUtils {
@@ -212,42 +254,34 @@ declare const excelUtils: ExcelUtils;
 
 class Test {
     aaa: string;
-
     bbb: number;
-
-    ccc: boolean;
+    ccc: Date;
 }
 
 const excelReaderConfig = new ExcelReaderConfig<Test>();
 
-excelReaderConfig.heads = {
+excelReaderConfig.columns = {
     aaa: {
-        column: "aaa",
-        dateTimeFormat: {
-            value: "yyyy-MM-dd HH:mm:ss"
-        }
+        dataType: DataType.String,
+        column: "第一列",
+        dateFormat: "yyyy-MM-dd HH:mm:ss",
     },
     bbb: {
-        column: "bbb",
+        dataType: DataType.BigDecimal,
+        column: "第二列",
     },
     ccc: {
-        column: "ccc",
+        dataType: DataType.Date,
+        column: "第三列",
     }
 }
 
 excelUtils.read<Test>(excelReaderConfig).sheet(0).doRead();
 
 excelUtils.read<Test>({
-    autoCloseStream: false,
-    extraRead: [],
-    ignoreEmptyRow: false,
-    mandatoryUseInputStream: false,
     sheet: 0,
     headRowNumber: 1,
-    useScientificFormat: false,
-    registerConverter: [],
-    use1904windowing: false,
-    locale: Locale.SIMPLIFIED_CHINESE,
+    ignoreEmptyRow: false,
     autoTrim: false,
 }).sheet(0).doRead();
 
