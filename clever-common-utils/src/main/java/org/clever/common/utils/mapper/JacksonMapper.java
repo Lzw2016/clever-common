1f11d1a20d20e15afc5c974c7704a244ebdc57cf
lizw <lzw1000000@163.com>
2020-10-19 11:44:49 +0800

    优化 JacksonMapper.java

--- clever-common-utils/src/main/java/org/clever/common/utils/mapper/JacksonMapper.java
+++ clever-common-utils/src/main/java/org/clever/common/utils/mapper/JacksonMapper.java
@@ -57,40 +57,7 @@ public class JacksonMapper {
      * 通用配置,参考 Jackson2ObjectMapperBuilder
      */
     private JacksonMapper() {
-        final String dateFormatPattern = "yyyy-MM-dd HH:mm:ss";
-        final ClassLoader moduleClassLoader = getClass().getClassLoader();
-        // 创建 ObjectMapper
-        mapper = new ObjectMapper();
-        // 设置输入时忽略在JSON字符串中存在但Java对象实际没有的属性
-        mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
-        // 允许单引号、允许不带引号的字段名称
-        mapper.configure(JsonParser.Feature.ALLOW_SINGLE_QUOTES, true);
-        mapper.configure(JsonParser.Feature.ALLOW_UNQUOTED_FIELD_NAMES, true);
-        // 使用枚举的的toString函数来读写枚举
-        // mapper.enable(SerializationFeature.WRITE_ENUMS_USING_TO_STRING);
-        // mapper.enable(DeserializationFeature.READ_ENUMS_USING_TO_STRING);
-        // 设置时区 getTimeZone("GMT+8")
-        mapper.setTimeZone(TimeZone.getTimeZone("GMT+8"));
-        // locale: zh_CN
-        mapper.setLocale(Locale.CHINA);
-        // 设置时间格式
-        mapper.setDateFormat(new SimpleDateFormat(dateFormatPattern));
-        // 注册 Module
-        List<Module> modules = new ArrayList<>();
-        MultiValueMap<Object, Module> modulesToRegister = new LinkedMultiValueMap<>();
-        ObjectMapper.findModules(moduleClassLoader).forEach(module -> registerModule(module, modulesToRegister));
-        registerWellKnownModulesIfAvailable(modulesToRegister, moduleClassLoader);
-        for (List<Module> nestedModules : modulesToRegister.values()) {
-            modules.addAll(nestedModules);
-        }
-        SimpleModule module = new SimpleModule();
-        modules.add(module);
-        module.addSerializer(DateTime.class, new DateTimeSerializer(new JacksonJodaDateFormat(DateTimeFormat.forPattern(dateFormatPattern).withZoneUTC()), 0));
-        module.addSerializer(BigInteger.class, ToStringSerializer.instance);
-        module.addSerializer(Long.class, ToStringSerializer.instance);
-        module.addSerializer(Long.TYPE, ToStringSerializer.instance);
-        module.addDeserializer(Date.class, CustomizerDateDeserializer.instance);
-        mapper.registerModules(modules);
+        mapper = newObjectMapper();
     }
 
     /**
@@ -98,58 +65,6 @@ public class JacksonMapper {
      */
     public static JacksonMapper getInstance() {
         return Instance;
-    }
-
-    private void registerModule(Module module, MultiValueMap<Object, Module> modulesToRegister) {
-        if (module.getTypeId() == null) {
-            modulesToRegister.add(SimpleModule.class.getName(), module);
-        } else {
-            modulesToRegister.set(module.getTypeId(), module);
-        }
-    }
-
-    @SuppressWarnings("unchecked")
-    private void registerWellKnownModulesIfAvailable(MultiValueMap<Object, Module> modulesToRegister, ClassLoader moduleClassLoader) {
-        try {
-            Class<? extends Module> jdk8ModuleClass = (Class<? extends Module>) ClassUtils.forName("com.fasterxml.jackson.datatype.jdk8.Jdk8Module", moduleClassLoader);
-            Module jdk8Module = BeanUtils.instantiateClass(jdk8ModuleClass);
-            modulesToRegister.set(jdk8Module.getTypeId(), jdk8Module);
-        } catch (ClassNotFoundException ex) {
-            // jackson-datatype-jdk8 not available
-        }
-
-        try {
-            Class<? extends Module> javaTimeModuleClass = (Class<? extends Module>) ClassUtils.forName("com.fasterxml.jackson.datatype.jsr310.JavaTimeModule", moduleClassLoader);
-            Module javaTimeModule = BeanUtils.instantiateClass(javaTimeModuleClass);
-            modulesToRegister.set(javaTimeModule.getTypeId(), javaTimeModule);
-        } catch (ClassNotFoundException ex) {
-            // jackson-datatype-jsr310 not available
-        }
-
-        // Joda-Time present?
-        if (ClassUtils.isPresent("org.joda.time.LocalDate", moduleClassLoader)) {
-            try {
-                Class<? extends Module> jodaModuleClass = (Class<? extends Module>) ClassUtils.forName("com.fasterxml.jackson.datatype.joda.JodaModule", moduleClassLoader);
-                Module jodaModule = BeanUtils.instantiateClass(jodaModuleClass);
-                modulesToRegister.set(jodaModule.getTypeId(), jodaModule);
-            } catch (ClassNotFoundException ex) {
-                // jackson-datatype-joda not available
-            }
-        }
-
-        // Kotlin present?
-        if (KotlinDetector.isKotlinPresent()) {
-            try {
-                Class<? extends Module> kotlinModuleClass = (Class<? extends Module>) ClassUtils.forName("com.fasterxml.jackson.module.kotlin.KotlinModule", moduleClassLoader);
-                Module kotlinModule = BeanUtils.instantiateClass(kotlinModuleClass);
-                modulesToRegister.set(kotlinModule.getTypeId(), kotlinModule);
-            } catch (ClassNotFoundException ex) {
-                if (!kotlinWarningLogged) {
-                    kotlinWarningLogged = true;
-                    log.warn("For Jackson Kotlin classes support please add \"com.fasterxml.jackson.module:jackson-module-kotlin\" to the classpath");
-                }
-            }
-        }
     }
 
     /**
@@ -269,5 +184,95 @@ public class JacksonMapper {
      */
     public ObjectMapper getMapper() {
         return mapper;
+    }
+
+    public static ObjectMapper newObjectMapper() {
+        final String dateFormatPattern = "yyyy-MM-dd HH:mm:ss";
+        final ClassLoader moduleClassLoader = JacksonMapper.class.getClassLoader();
+        // 创建 ObjectMapper
+        ObjectMapper mapper = new ObjectMapper();
+        // 设置输入时忽略在JSON字符串中存在但Java对象实际没有的属性
+        mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+        // 允许单引号、允许不带引号的字段名称
+        mapper.configure(JsonParser.Feature.ALLOW_SINGLE_QUOTES, true);
+        mapper.configure(JsonParser.Feature.ALLOW_UNQUOTED_FIELD_NAMES, true);
+        // 使用枚举的的toString函数来读写枚举
+        // mapper.enable(SerializationFeature.WRITE_ENUMS_USING_TO_STRING);
+        // mapper.enable(DeserializationFeature.READ_ENUMS_USING_TO_STRING);
+        // 设置时区 getTimeZone("GMT+8")
+        mapper.setTimeZone(TimeZone.getTimeZone("GMT+8"));
+        // locale: zh_CN
+        mapper.setLocale(Locale.CHINA);
+        // 设置时间格式
+        mapper.setDateFormat(new SimpleDateFormat(dateFormatPattern));
+        // 注册 Module
+        List<Module> modules = new ArrayList<>();
+        MultiValueMap<Object, Module> modulesToRegister = new LinkedMultiValueMap<>();
+        ObjectMapper.findModules(moduleClassLoader).forEach(module -> registerModule(module, modulesToRegister));
+        registerWellKnownModulesIfAvailable(modulesToRegister, moduleClassLoader);
+        for (List<Module> nestedModules : modulesToRegister.values()) {
+            modules.addAll(nestedModules);
+        }
+        SimpleModule module = new SimpleModule();
+        modules.add(module);
+        module.addSerializer(DateTime.class, new DateTimeSerializer(new JacksonJodaDateFormat(DateTimeFormat.forPattern(dateFormatPattern).withZoneUTC()), 0));
+        module.addSerializer(BigInteger.class, ToStringSerializer.instance);
+        module.addSerializer(Long.class, ToStringSerializer.instance);
+        module.addSerializer(Long.TYPE, ToStringSerializer.instance);
+        module.addDeserializer(Date.class, CustomizerDateDeserializer.instance);
+        mapper.registerModules(modules);
+        return mapper;
+    }
+
+    private static void registerModule(Module module, MultiValueMap<Object, Module> modulesToRegister) {
+        if (module.getTypeId() == null) {
+            modulesToRegister.add(SimpleModule.class.getName(), module);
+        } else {
+            modulesToRegister.set(module.getTypeId(), module);
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    private static void registerWellKnownModulesIfAvailable(MultiValueMap<Object, Module> modulesToRegister, ClassLoader moduleClassLoader) {
+        try {
+            Class<? extends Module> jdk8ModuleClass = (Class<? extends Module>) ClassUtils.forName("com.fasterxml.jackson.datatype.jdk8.Jdk8Module", moduleClassLoader);
+            Module jdk8Module = BeanUtils.instantiateClass(jdk8ModuleClass);
+            modulesToRegister.set(jdk8Module.getTypeId(), jdk8Module);
+        } catch (ClassNotFoundException ex) {
+            // jackson-datatype-jdk8 not available
+        }
+
+        try {
+            Class<? extends Module> javaTimeModuleClass = (Class<? extends Module>) ClassUtils.forName("com.fasterxml.jackson.datatype.jsr310.JavaTimeModule", moduleClassLoader);
+            Module javaTimeModule = BeanUtils.instantiateClass(javaTimeModuleClass);
+            modulesToRegister.set(javaTimeModule.getTypeId(), javaTimeModule);
+        } catch (ClassNotFoundException ex) {
+            // jackson-datatype-jsr310 not available
+        }
+
+        // Joda-Time present?
+        if (ClassUtils.isPresent("org.joda.time.LocalDate", moduleClassLoader)) {
+            try {
+                Class<? extends Module> jodaModuleClass = (Class<? extends Module>) ClassUtils.forName("com.fasterxml.jackson.datatype.joda.JodaModule", moduleClassLoader);
+                Module jodaModule = BeanUtils.instantiateClass(jodaModuleClass);
+                modulesToRegister.set(jodaModule.getTypeId(), jodaModule);
+            } catch (ClassNotFoundException ex) {
+                // jackson-datatype-joda not available
+            }
+        }
+
+        // Kotlin present?
+        if (KotlinDetector.isKotlinPresent()) {
+            try {
+                Class<? extends Module> kotlinModuleClass = (Class<? extends Module>) ClassUtils.forName("com.fasterxml.jackson.module.kotlin.KotlinModule", moduleClassLoader);
+                Module kotlinModule = BeanUtils.instantiateClass(kotlinModuleClass);
+                modulesToRegister.set(kotlinModule.getTypeId(), kotlinModule);
+            } catch (ClassNotFoundException ex) {
+                if (!kotlinWarningLogged) {
+                    kotlinWarningLogged = true;
+                    log.warn("For Jackson Kotlin classes support please add \"com.fasterxml.jackson.module:jackson-module-kotlin\" to the classpath");
+                }
+            }
+        }
     }
 }
