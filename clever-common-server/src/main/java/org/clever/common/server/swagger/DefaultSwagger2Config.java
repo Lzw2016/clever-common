1dc11683d862434874667df080b121c020bdcbb7
lizw <1183409807@qq.com>
2020-12-20 20:23:53 +0800

    发版 2.0.3-SNAPSHOT

--- clever-common-server/src/main/java/org/clever/common/server/swagger/DefaultSwagger2Config.java
+++ clever-common-server/src/main/java/org/clever/common/server/swagger/DefaultSwagger2Config.java
@@ -9,7 +9,7 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * 作者： lzw<br/>
@@ -17,7 +17,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
  */
 @Profile({"dev", "test"})
 @Configuration
-@EnableSwagger2WebMvc
+@EnableSwagger2
 public class DefaultSwagger2Config {
 
     @Bean
